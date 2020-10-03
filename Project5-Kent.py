670b82cb5e3dde496e086ec15d00a69a09626bde
Jeff-Kent <72316104+Jeff-Kent@users.noreply.github.com>
2020-10-03 13:37:38 -0500

    Add files via upload

---
+++ Project5-Kent.py
@@ -0,0 +1,4 @@
+base = int(input("Enter the base of a triangle: "))
+height = int(input("Enter the height of a triagle: "))
+area = .5 * base * height
+print("The area of the triangle is", area, "square units.")
---
+++ Project6-Kent.py
@@ -0,0 +1,4 @@
+radius = float(input("Enter the radius a circle: "))
+area = 3.14 * radius ** 2
+print("The area of the circle is", area, "square units.")
+             
